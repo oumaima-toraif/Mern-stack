09c48f95e933fb4c387e76c5dd306a50bd9faf2d
Oumaima TORAIF <69909159+oumaima-toraif@users.noreply.github.com>
2022-04-08 00:29:31 +0000

    add the files

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Inputstudent from './components/inputstudent';
+import Liststudent from './components/liststudent';
+function App() {
+  return (
+    <div>
+    <Inputstudent/>
+    <Liststudent/>
+    </div>
+  );
+}
+
+export default App;
